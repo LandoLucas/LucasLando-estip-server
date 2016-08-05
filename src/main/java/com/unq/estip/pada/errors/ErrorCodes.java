6ed2522a392529277b4942d489b4ec3e54a6ee1a
Lucas Lando <lucas.emanuel.lando@everis.com>
2016-08-05 17:20:27 -0300

    Adding error message when a store is in use and the user wants to delete it

--- src/main/java/com/unq/estip/pada/errors/ErrorCodes.java
+++ src/main/java/com/unq/estip/pada/errors/ErrorCodes.java
@@ -3,6 +3,7 @@ package com.unq.estip.pada.errors;
 public enum ErrorCodes {
 
 	PRODUCT_IN_USE,
-	CLIENT_IN_USE;
+	CLIENT_IN_USE,
+	STORE_IN_USE;
 	
 }
--- src/main/java/com/unq/estip/pada/rest/StoresRest.java
+++ src/main/java/com/unq/estip/pada/rest/StoresRest.java
@@ -10,8 +10,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.unq.estip.pada.errors.ErrorCodes;
 import com.unq.estip.pada.model.Store;
 import com.unq.estip.pada.service.StoreService;
 
@@ -47,8 +49,12 @@ public class StoresRest {
 	@Path("/remove")
 	@Consumes("application/x-www-form-urlencoded")
 	public Response removeStore(@FormParam("id") String id) {
-		storeService.removeStore(Integer.valueOf(id));
-		return Response.ok().header("Access-Control-Allow-Origin", "*").build();
+		try{
+			storeService.removeStore(Integer.valueOf(id));
+			return Response.ok().header("Access-Control-Allow-Origin", "*").build();
+		}catch(DataIntegrityViolationException e){
+			return Response.serverError().header("Access-Control-Allow-Origin", "*").entity(ErrorCodes.STORE_IN_USE).build();
+		}
 	}
 	
 }
